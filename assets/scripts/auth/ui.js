b6d684f4ac0b023fc09cf52b63af3871bf660d8b
oliversablove <osablove@gmail.com>
2019-06-26 09:55:02 -0400

    Add User Auth with Test Scripts
    - Added HTML user auth buttons
    - Add User auth logic (app.js, api.js, events.js)
    - Add curl scripts for user auth testing

---
+++ assets/scripts/auth/ui.js
@@ -0,0 +1,38 @@
+'use strict'
+
+const store = require('../store')
+
+const onSignUpSuccess = responseData => {
+}
+
+const onSignUpFailure = responseData => {
+}
+
+const onSignInSuccess = responseData => {
+}
+
+const onSignInFailure = responseData => {
+}
+
+const onSignOutSuccess = () => {
+}
+
+const onSignOutFailure = () => {
+}
+
+const onChangePasswordSuccess = () => {
+}
+
+const onChangePasswordFailure = () => {
+}
+
+module.exports = {
+  onSignUpFailure,
+  onSignUpSuccess,
+  onSignInFailure,
+  onSignInSuccess,
+  onSignOutFailure,
+  onSignOutSuccess,
+  onChangePasswordFailure,
+  onChangePasswordSuccess
+}
